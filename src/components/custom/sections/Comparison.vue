29ead53c069f8abab943646abe1b868f5965ca04
samaradel <samar@incubaid.com>
2021-02-18 14:03:06 +0200

    Fix img

--- src/components/custom/sections/Comparison.vue
+++ src/components/custom/sections/Comparison.vue
@@ -12,7 +12,7 @@
       >
         <g-image
           class="mx-auto mb-8"
-          :src="section.image"
+          :src="img(section.image)"
           :alt="section.title"
         />
         <h3 class="text-2xl mb-4 font-semibold font-heading">
@@ -28,7 +28,7 @@
     <div>
       <g-link
         v-if="main.button"
-        class="bg-blue-900 text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 rounded shadow rounded-full" 
+        class="bg-blue-900 text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 rounded shadow rounded-full"
         :to="main.link"
         >{{ main.button }}</g-link
       >
@@ -39,5 +39,12 @@
 <script>
 export default {
   props: ["main", "sections"],
+  methods: {
+    img(image) {
+      if (!image) return "";
+      if (image.src) return image.src;
+      return image;
+    },
+  },
 };
 </script>
